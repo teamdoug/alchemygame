42827343cba31664c1376629b9733af3f8b85b43
Doug Ellwanger <doug.ellwanger@gmail.com>
2022-07-24 17:49:21 -0700

    Copy basic styling/functionality from lightgame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,228 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const debug = false;
+const maxWidth = 2000;
+const maxHeight = 1500;
+const centerX = maxWidth / 2;
+const centerY = maxHeight / 2;
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.lastFrame = window.performance.now();
+    this.lastSave = window.performance.now();
+    this.canvas = React.createRef();
+    this.width = 0;
+    const storedState = localStorage.getItem("heartosisIGJ5Save");
+    if (storedState) {
+      this.state = JSON.parse(storedState);
+    } else {
+      this.state = this.getInitState();
+    }
+    this.resetLocalVars();
+  }
+
+  resetLocalVars = () => {
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.mouseClicked = false;
+  };
+
+  getInitState = () => {
+    let state = {
+      paused: false,
+    }
+    return state;
+  }
+
+  reset = () => {
+    this.confirmingReset = false;
+    let state = this.getInitState();
+    this.setState(state, this.resizeCanvas);
+  }
+
+  render() {
+    let s = this.state;
+
+    const canvas = this.canvas.current;
+
+    if (canvas !== null) {
+      this.drawCanvas(
+        canvas,
+      );
+    }
+
+    return (
+      <div id="verticalFlex">
+        <div className="header">
+          <div className="headerTabs">
+          </div>
+          <div className="headerButtons">
+
+          </div>
+        </div>
+        <div id="flex">
+          <div className="panel" id="leftPanel">
+          </div>
+          <div className="panel" id="mainPanel">
+            <canvas
+              ref={this.canvas}
+              onMouseMove={this.mouseMove}
+              onMouseDown={this.mouseMove}
+              onMouseUp={this.mouseMove}
+              onMouseLeave={this.mouseMove}
+              onTouchStart={this.touchMove}
+              onTouchMove={this.touchMove}
+              onTouchEnd={this.touchMove}
+              onTouchCancel={this.touchMove}
+            ></canvas>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  drawCanvas = (canvas) => {
+
+  }
+
+  mouseMove = (e) => {
+    if (!this.canvas.current) {
+      return;
+    }
+    if (e.type === "mouseleave") {
+      this.mouseClicked = false;
+      return;
+    }
+    if ((e.buttons & 1) === 1) {
+      var rect = this.canvas.current.getBoundingClientRect();
+      this.mouseClicked = true;
+      this.mouseX = e.clientX - rect.left;
+      this.mouseY = e.clientY - rect.top;
+    } else {
+      this.mouseClicked = false;
+    }
+  };
+
+  touchMove = (e) => {
+    if (!this.canvas.current) {
+      return;
+    }
+
+    if (e.type === "touchcancel" || e.type === "touchend") {
+      this.mouseClicked = false;
+      return;
+    }
+    if (e.touches.length > 0) {
+      var rect = this.canvas.current.getBoundingClientRect();
+      this.mouseClicked = true;
+      this.mouseX =
+        e.touches[0].clientX - rect.left;
+      this.mouseY =
+        e.touches[0].clientY - rect.top;
+    } else {
+      this.mouseClicked = false;
+    }
+  };
+
+  resizeCanvas = () => {
+    if (this.canvas.current !== null) {
+      this.canvas.current.style.width = "100%";
+      this.canvas.current.style.height = "100%";
+      if (this.canvas.current.offsetWidth > maxWidth) {
+        this.canvas.current.width = maxWidth;
+        this.canvas.current.style.width = maxWidth + "px";
+        console.log('maxwidth ' + maxWidth);
+      } else {
+        this.canvas.current.width = this.canvas.current.offsetWidth;
+        console.log('offsetwidth ' + this.canvas.current.width);
+      }
+      if (this.canvas.current.offsetHeight > maxHeight) {
+        this.canvas.current.height = maxHeight;
+        this.canvas.current.style.height = maxHeight + "px";
+      } else {
+        this.canvas.current.height = this.canvas.current.offsetHeight;
+      }
+      this.width = this.canvas.current.width;
+    }
+
+    if (this.state.paused) {
+      this.forceUpdate();
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("beforeunload", this.save);
+    window.addEventListener("resize", this.resizeCanvas);
+    this.resizeCanvas();
+    this.renderID = window.requestAnimationFrame(this.gameLoop);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("beforeunload", this.save);
+    window.cancelAnimationFrame(this.renderID);
+  }
+
+  gameLoop = (tFrame) => {
+    if (tFrame > this.lastSave + 10000) {
+      this.save();
+      this.lastSave = tFrame;
+    }
+    let delta = tFrame - this.lastFrame;
+    if (delta > 1000) {
+      if (debug) {
+        console.log("delta too large: " + delta);
+      }
+      delta = 1000;
+    }
+
+    let minDelta = 1000 / 60;
+    let debugFrame = false;
+    if (tFrame % 1000 < minDelta) {
+      debugFrame = true;
+    }
+    let loopCount = 0;
+    while (delta > minDelta) {
+      delta -= minDelta;
+      if (this.state.paused) {
+        continue;
+      }
+      loopCount += 1;
+      this.update(minDelta, debugFrame);
+      debugFrame = false;
+    }
+    if (debug && loopCount > 1) {
+      console.log("loops", loopCount);
+    }
+    this.lastFrame = tFrame - delta;
+    this.renderID = window.requestAnimationFrame(this.gameLoop);
+  };
+
+  update = (delta, debugFrame) => {
+    let s = this.state;
+    let relDelta = delta / 1000;
+    let updates = {}
+    let forceResize = false;
+    let callback = forceResize ? this.resizeCanvas : () => { };
+    this.setState(updates, callback);
+
+  }
+
+
+  togglePause = () => {
+    this.setState({ paused: !this.state.paused });
+  };
+
+  save = () => {
+    localStorage.setItem("heartosisIGJ5Save", JSON.stringify(this.state));
+  };
+
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 export default App;
